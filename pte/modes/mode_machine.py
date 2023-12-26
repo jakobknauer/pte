5163a7b66b9da0f9bf908c3aae9738510390a92d
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2023-12-26 16:26:10 +0100

    Use logging in main and ModeMachine

--- pte/modes/mode_machine.py
+++ pte/modes/mode_machine.py
@@ -8,7 +8,7 @@ class ModeMachine:
     def __init__(self, *modes: Mode, logger: logging.Logger | None = None) -> None:
         self._mode: Mode | None = None
         self._modes = {state.name: state for state in modes}
-        self._logger = logger or logging.getLogger()
+        self._logger = logger or logging.getLogger(__name__)
 
     def switch_mode(self, new_mode: Mode | None) -> None:
         old_mode = self._mode
@@ -23,47 +23,50 @@ class ModeMachine:
         if new_mode:
             new_mode.enter()
 
-        self.info(f"Transitioned from {old_mode} to {new_mode}.")
+        self.debug(f"Transitioned from {old_mode} to {new_mode}.")
 
     def run(self) -> None:
         self.info(f"Initial mode: {self._mode}.")
 
         while self._mode:
-            self.info(f"Draw {self._mode}.")
+            self.debug(f"Draw {self._mode}.")
             try:
                 self._mode.draw()
             except:
                 self.error(f"An error occured in {self._mode}.draw().", exc_info=True)
                 raise
 
-            self.info(f"Update {self._mode}.")
+            self.debug(f"Update {self._mode}.")
             try:
                 transition = self._mode.update()
             except:
-                self.error(
-                    f"An error occured in {self._mode}.update().", exc_info=True
-                )
+                self.error(f"An error occured in {self._mode}.update().", exc_info=True)
                 raise
 
             match transition:
                 case TransitionType.STAY:
-                    self.info(f"Stay in {self._mode}.")
+                    self.debug(f"Stay in {self._mode}.")
                     continue
                 case (TransitionType.SWITCH, str(name)) if name in self._modes:
                     new_mode = self._modes[name]
                     self.switch_mode(new_mode)
                     continue
                 case (TransitionType.SWITCH, str(name)):
-                    self.error(f"Transition to unknown mode with name '{name}' requested.")
+                    self.error(
+                        f"Transition to unknown mode with name '{name}' requested."
+                    )
                     return
                 case TransitionType.QUIT | None:
-                    self.info(f"Quit from final mode {self._mode}")
+                    self.debug(f"Quit from final mode {self._mode}")
                     break
                 case _:
                     self.error(f"Invalid transition '{transition}' requested.")
                     return
 
         self.info(f"Final mode: {self._mode}.")
+
+    def debug(self, *args, **kwargs) -> None:
+        self._logger.debug(*args, **kwargs)
 
     def info(self, *args, **kwargs) -> None:
         self._logger.info(*args, **kwargs)
--- pte/run.py
+++ pte/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import curses
+import logging
 
 from pte import modes
 from pte.text_buffer import TextBuffer
@@ -8,7 +9,26 @@ from pte.view import MainView
 from pte.command_buffer import CommandBuffer
 
 
+log = logging.getLogger(__name__)
+
+
+def set_up_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler("log/log.txt")
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+
 def main(stdscr: curses.window):
+    log.info("Starting up...")
+
+    log.info("Setting up view...")
+
     curses.set_escdelay(1)
     curses.use_default_colors()
     for i in range(0, curses.COLORS):
@@ -17,17 +37,26 @@ def main(stdscr: curses.window):
     view = MainView(stdscr)
     command_buffer = CommandBuffer(stdscr)
 
+    log.info("Reading file...")
+
     with open("example.txt") as fp:
         text_buffer = TextBuffer.from_file(fp)
+
+    log.info("Setting up modes...")
 
     normal = modes.NormalMode(text_buffer, view, command_buffer)
     insert = modes.InsertMode(text_buffer, view, command_buffer)
     command = modes.CommandMode(text_buffer, view, command_buffer)
 
+    log.info("Start mode machine.")
+
     mode_machine = modes.ModeMachine(normal, insert, command)
     mode_machine.switch_mode(normal)
     mode_machine.run()
 
+    log.info("Leaving...")
+
 
 if __name__ == "__main__":
+    set_up_logging()
     curses.wrapper(main)
