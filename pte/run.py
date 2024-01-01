c0023a5dccbb9f9b8ffbe484a36cc07b47f3ff24
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2024-01-01 10:28:05 +0100

    Log to XDG_DATA_HOME

--- pte/run.py
+++ pte/run.py
@@ -3,6 +3,8 @@
 import argparse
 import curses
 import logging
+import os
+from pathlib import Path
 
 from pte import modes
 from pte.text_buffer import TextBuffer
@@ -16,11 +18,16 @@ log = logging.getLogger(__name__)
 def set_up_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    fh = logging.FileHandler("log/log.txt", "w+")
-    fh.setLevel(logging.INFO)
+
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
+
+    log_directory = Path(os.environ["XDG_DATA_HOME"]) / "pte" / "log"
+    log_directory.mkdir(parents=True, exist_ok=True)
+
+    fh = logging.FileHandler(log_directory / "log.txt", "w+")
+    fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
